7c60f5df0c412b49972cee24405be5da3a4d124e
volodavkaa <volodavkaa@gmail.com>
2024-11-14 13:28:44 +0200

    3 - робота над css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
+import '../styles/Navbar.css';
 
 const Navbar = () => {
   const location = useLocation();
@@ -11,27 +12,31 @@ const Navbar = () => {
     navigate('/login');
   };
 
-  
   if (location.pathname === '/login' || location.pathname === '/register') {
     return null;
   }
 
   return (
-    <nav>
+    <div className="navbar-container">
       <div className="logo">SnapTalk</div>
-      <ul>
-        <li><Link to="/posts">Пости</Link></li>
-        <li><Link to="/chat">Чат</Link></li>
-        {!token ? (
-          <>
-            <li><Link to="/register">Реєстрація</Link></li>
-            <li><Link to="/login">Авторизація</Link></li>
-          </>
-        ) : (
-          <li><button onClick={handleLogout}>Вихід</button></li>
-        )}
+      <ul className="nav-links">
+        <li>
+          <Link to="/posts">
+            <i className="fas fa-file-alt"></i> Пости
+          </Link>
+        </li>
+        <li>
+          <Link to="/chat">
+            <i className="fas fa-comments"></i> Чат
+          </Link>
+        </li>
       </ul>
-    </nav>
+      {token && (
+        <button className="logout-button" onClick={handleLogout}>
+          <i className="fas fa-sign-out-alt"></i> Вихід
+        </button>
+      )}
+    </div>
   );
 };
 
